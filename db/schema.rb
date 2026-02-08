b007f6c4c00c1d232a6aa8c9e8a7c3972fc1746f
jokerlay000-design <jokerlay000@gmail.com>
2026-02-08 17:10:52 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_08_064643) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_08_080957) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
